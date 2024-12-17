10d29aa6d181303440aca6e6dab736e5aec107de
Dante Viegas <dante.viegas1@gmail.com>
2024-12-17 20:40:07 -0300

    print.py
    How to use the tool print in Python.

---
+++ print.py
@@ -0,0 +1,4 @@
+#the tool "print" is used to display an information on the terminal utilizing the "()" as parameter.
+print("Bye World!")
+#to display texts, you have to use "". SO, from the example above, the text shown will be "Bye World!". 
+#print can also idsplay other things besides texts.
