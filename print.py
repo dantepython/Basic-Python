0170b16696350af15d3dee3f39650b9f6778e5cf
Dante Viegas <dante.viegas1@gmail.com>
2024-12-17 20:56:58 -0300

    print.py

--- print.py
+++ print.py
@@ -1,4 +1,1 @@
-#the tool "print" is used to display an information on the terminal utilizing the "()" as parameter.
-#to display texts, you have to use "". SO, from the example above, the text shown will be "Bye World!". 
-#print can also idsplay other things besides texts.
-print("Bye World!")
+
